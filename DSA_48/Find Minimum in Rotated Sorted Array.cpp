bf85940dc872c3b2ccfe027cccd39060000ba5e1
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-09-25 19:26:27 +0530

    Update Find Minimum in Rotated Sorted Array.cpp
    Signed-off-by: Sanjay <121040100+sanjay-1458@users.noreply.github.com>

--- DSA_48/Find Minimum in Rotated Sorted Array.cpp
+++ DSA_48/Find Minimum in Rotated Sorted Array.cpp
@@ -27,3 +27,24 @@ int findMin(vector<int>& arr)
 		}
 	}
 }
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n=nums.size();
+        int ans=INT_MAX;
+        int low=0,high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]<=nums[high]){
+                ans=min(ans,nums[mid]);
+                high=mid-1;
+            }
+            else if(nums[mid]>=nums[low]){
+                ans=min(ans,nums[mid]);
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
