0f95699eeff668b34e3c0b9752383bfd375f229c
AYUSH KUSHWAHA <113614946+Ayushkushwaha0512@users.noreply.github.com>
2023-09-25 19:03:14 +0530

    Create Find the DifferenceBM.java
    Signed-off-by: AYUSH KUSHWAHA <113614946+Ayushkushwaha0512@users.noreply.github.com>

---
+++ DSA_13/Find the DifferenceBM.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class solution{
+	public char findTheDifference(String s, String t) {
+        char result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            result ^= s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            result ^= t.charAt(i);
+        }
+        return result;
+	}
+
+	public static void main(String args[]){
+		String s=abcd;
+		String t=abcde;
+		System.out.println(findtheDifference);
+	}
+	
+}
