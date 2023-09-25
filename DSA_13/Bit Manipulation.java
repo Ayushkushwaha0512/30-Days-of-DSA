96186114140c0839d08e14b0107c0e650ec25e8b
AYUSH KUSHWAHA <113614946+Ayushkushwaha0512@users.noreply.github.com>
2023-09-25 19:42:41 +0530

    Create Bit Manipulation.java
    Signed-off-by: AYUSH KUSHWAHA <113614946+Ayushkushwaha0512@users.noreply.github.com>

---
+++ DSA_13/Bit Manipulation.java
@@ -0,0 +1,8 @@
+public class bitmanipulation {
+    public static void main(String args[]) {
+        System.out.println(5 & 6);// binary AND
+        System.out.println(5 | 6);// Binary OR
+        System.out.println(5 ^ 6);// Binary XORch
+        System.out.println(~5);// Binary one'complement
+    }
+}
