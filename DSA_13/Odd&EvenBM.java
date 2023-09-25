140403aa7a6c66ca3577bcf8d7009455ceab06bd
AYUSH KUSHWAHA <113614946+Ayushkushwaha0512@users.noreply.github.com>
2023-09-25 19:51:01 +0530

    Delete DSA_13/OddOrEvenBM.java
    Signed-off-by: AYUSH KUSHWAHA <113614946+Ayushkushwaha0512@users.noreply.github.com>

--- DSA_13/Odd&EvenBM.java
+++
@@ -1,9 +0,0 @@
-public static void oddOREven(int n){
-  int bitMask=1;
-  if((n & bitMask)==0){
-      System.out.println("even number");
-  }
-  else{
-    System.out.println("odd number");
-  }
-}
